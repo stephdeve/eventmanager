fbcc19d4c77cc19679c384b156a06e760fe58cb0
stephdeve <stephdev6@gmail.com>
2025-10-18 20:47:24 +0200

    Ajout des fonctionnalités techniques7

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -62,7 +62,10 @@
                             </span>
                         </div>
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> b8b367ed66883b8ad0003dc508512480dd6986b1
                         <div class="flex items-center gap-2">
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
